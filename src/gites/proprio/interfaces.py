5def840b4f6d788a7e04c8e6a816f7f326597b89
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-03-19 14:23:32 +0100

    - Imports the view from gites.skin for the hosting edition.

---
+++ src/gites/proprio/interfaces.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+"""
+gites.proprio
+
+Created by mpeeters
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+
+import zope.interface
+
+
+class IHebergementInfo(zope.interface.Interface):
+    """ Mise à jour info hebergement """
+
+    def addHebergementMaj():
+        """
+        Ajoute les infos mise à jour par de l'hébergement le prorpio dans
+        la table provisoire
+        """
