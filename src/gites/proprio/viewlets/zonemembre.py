0337406ef4b222d6eb74a0e79cf24f9b62d1bd6d
Laurent Lasudry <lasudry@gmail.com>
2013-05-22 10:10:11 +0200

    Merge proprio viewlets into one viewlet

--- src/gites/proprio/viewlets/zonemembre.py
+++ src/gites/proprio/viewlets/zonemembre.py
@@ -18,7 +18,7 @@ class ZoneMembreViewletManager(grok.ViewletManager):
         return "zone-membre" in self.context.getPhysicalPath()
 
 
-class CalendrierViewlet(grok.Viewlet):
+class ZoneMembreTopMenuViewlet(grok.Viewlet):
     grok.order(10)
 
     def isVisible(self):
@@ -52,17 +52,6 @@ class CalendrierViewlet(grok.Viewlet):
             if heb.heb_calendrier_proprio == 'bloque':
                 return True
         return False
-
-    @memoize
-    def getGitesForProprio(self):
-        return getUtility(IVocabularyFactory, name='proprio.hebergements')(self.context)
-
-
-class MajInfosViewlet(grok.Viewlet):
-    grok.order(20)
-
-    def isVisible(self):
-        return len(self.getGitesForProprio()) > 0
 
     @memoize
     def getGitesForProprio(self):
