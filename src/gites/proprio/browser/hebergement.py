8086facf3c1be1abf5918e9120cd61e9f25843c8
schminitz <gillian@affinitic.be>
2014-06-26 17:32:23 +0200

    Remove old tarif form affinitic refs #5838

--- src/gites/proprio/browser/hebergement.py
+++ src/gites/proprio/browser/hebergement.py
@@ -101,38 +101,14 @@ class HebergementMixin(object):
             heb_maj_url=fields.get('heb_maj_url'),
             heb_maj_adresse=fields.get('heb_maj_adresse'),
             heb_maj_localite=fields.get('heb_maj_localite'),
-            heb_maj_tarif_we_bs=fields.get('heb_maj_tarif_we_bs'),
-            heb_maj_tarif_we_ms=fields.get('heb_maj_tarif_we_ms'),
-            heb_maj_tarif_we_hs=fields.get('heb_maj_tarif_we_hs'),
-            heb_maj_tarif_sem_bs=fields.get('heb_maj_tarif_sem_bs'),
-            heb_maj_tarif_sem_ms=fields.get('heb_maj_tarif_sem_ms'),
-            heb_maj_tarif_sem_hs=fields.get('heb_maj_tarif_sem_hs'),
-            heb_maj_tarif_garantie=fields.get('heb_maj_tarif_garantie'),
-            heb_maj_tarif_divers=fields.get('heb_maj_tarif_divers'),
             heb_maj_descriptif_fr=fields.get('heb_maj_descriptif_fr'),
             heb_maj_pointfort_fr=fields.get('heb_maj_pointfort_fr'),
-            heb_maj_taxe_montant=fields.get('heb_maj_taxe_montant'),
             heb_maj_forfait_montant=fields.get('heb_maj_forfait_montant'),
-            heb_maj_tarif_we_3n=fields.get('heb_maj_tarif_we_3n'),
-            heb_maj_tarif_we_4n=fields.get('heb_maj_tarif_we_4n'),
-            heb_maj_tarif_semaine_fin_annee=fields.get('heb_maj_tarif_semaine_fin_annee'),
             heb_maj_lit_1p=fields.get('heb_maj_lit_1p'),
             heb_maj_lit_2p=fields.get('heb_maj_lit_2p'),
             heb_maj_lit_sup=fields.get('heb_maj_lit_sup'),
             heb_maj_lit_enf=fields.get('heb_maj_lit_enf'),
             heb_maj_distribution_fr=fields.get('heb_maj_distribution_fr'),
-            heb_maj_tarif_chmbr_avec_dej_1p=fields.get('heb_maj_tarif_chmbr_avec_dej_1p'),
-            heb_maj_tarif_chmbr_avec_dej_2p=fields.get('heb_maj_tarif_chmbr_avec_dej_2p'),
-            heb_maj_tarif_chmbr_avec_dej_3p=fields.get('heb_maj_tarif_chmbr_avec_dej_3p'),
-            heb_maj_tarif_chmbr_sans_dej_1p=fields.get('heb_maj_tarif_chmbr_sans_dej_1p'),
-            heb_maj_tarif_chmbr_sans_dej_2p=fields.get('heb_maj_tarif_chmbr_sans_dej_2p'),
-            heb_maj_tarif_chmbr_sans_dej_3p=fields.get('heb_maj_tarif_chmbr_sans_dej_3p'),
-            heb_maj_tarif_chmbr_table_hote_1p=fields.get('heb_maj_tarif_chmbr_table_hote_1p'),
-            heb_maj_tarif_chmbr_table_hote_2p=fields.get('heb_maj_tarif_chmbr_table_hote_2p'),
-            heb_maj_tarif_chmbr_table_hote_3p=fields.get('heb_maj_tarif_chmbr_table_hote_3p'),
-            heb_maj_tarif_chmbr_autre_1p=fields.get('heb_maj_tarif_chmbr_autre_1p'),
-            heb_maj_tarif_chmbr_autre_2p=fields.get('heb_maj_tarif_chmbr_autre_2p'),
-            heb_maj_tarif_chmbr_autre_3p=fields.get('heb_maj_tarif_chmbr_autre_3p'),
             heb_maj_charge_fk=fields.get('heb_maj_charge_fk'))
         session.add(newEntry)
         session.flush()
@@ -178,8 +154,7 @@ class HebergementMixin(object):
         """ Adds record for the new metadatas that can be edited """
         editable = mappers.Metadata.get_editable()
         current = self.get_editable_metadata()
-        for metadata in [e for e in editable if e.met_pk not in [c.pk for c \
-                                                                 in current]]:
+        for metadata in [e for e in editable if e.met_pk not in [c.pk for c in current]]:
             link = mappers.LinkHebergementMetadata(
                 heb_fk=heb_pk,
                 metadata_fk=metadata.met_pk,
